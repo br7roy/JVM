7249356865299ad1230f22592beb1061e3c87a9a
Rust <br7roy@gmail.com>
2018-11-10 10:26:09 +0800

    commit

---
+++ src/main/java/com/rust/jvm/memorystructure/MethodAreaOOM.java
@@ -0,0 +1,25 @@
+/*
+* Package com.rust.jvm.memorystructure 
+* FileName: MethodAreaOOM
+* Author:   Rust
+* Date:     2018/7/23 14:31
+* Description: 
+* History: 
+*===============================================================================================
+*   author：          time：                             version：           desc：
+*   Rust                 2018/7/23  14:31             1.0                  
+*===============================================================================================
+*/
+package com.rust.jvm.memorystructure;
+
+import javax.management.MXBean;
+
+/**
+ * FileName:    MethodAreaOOM
+ * Author:      Rust
+ * Date:        2018/7/23
+ * Description: 方法区OOM
+ */
+@MXBean
+public interface MethodAreaOOM {
+}
---
+++ src/main/java/com/rust/jvm/memorystructure/OOMObject.java
@@ -0,0 +1,23 @@
+/*
+* Package com.rust.jvm.construct 
+* FileName: OOMObject
+* Author:   Rust
+* Date:     2018/7/23 13:52
+* Description: 
+* History: 
+*===============================================================================================
+*   author：          time：                             version：           desc：
+*   Rust                 2018/7/23  13:52             1.0                  
+*===============================================================================================
+*/
+package com.rust.jvm.memorystructure;
+
+/**
+ * FileName:    OOMObject
+ * Author:      Rust
+ * Date:        2018/7/23
+ * Description:
+ */
+public class OOMObject {
+
+}
